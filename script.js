bc418509f5a3459c05b65f1b3468479cfd2b958c
Aram Zucker-Scharff <azuckers@gmu.edu>
2018-09-30 17:54:49 -0400

    append child for new page

--- script.js
+++ script.js
@@ -4,9 +4,9 @@
 // prints "hi" in the browser's dev tools console
 console.log('hi');
 
-function pageAdd(){
+function pageAdd() {
   var page = document.getElementById('page-1');
   var page_clone = page.cloneNode(false);
   var paper = document.getElementById('paper');
-  
+  paper.appendChild(page_clone);
 }
