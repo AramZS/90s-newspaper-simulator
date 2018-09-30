e1f8d6d308306090868f3bc06543366efd3bf14f
Glitch (hello-webpage) <none>
2018-09-30 22:07:00 +0000

    🎱❤️ Checkpoint ./index.html:1908428/24 ./script.js:1908428/144 ./style.css:1908428/119

--- script.js
+++ script.js
@@ -4,9 +4,11 @@
 // prints "hi" in the browser's dev tools console
 console.log('hi');
 
-function pageAdd() {
+function pageAdd(){
   var page = document.getElementById('page-1');
   var page_clone = page.cloneNode(false);
   var paper = document.getElementById('paper');
+  page_clone.setAttribute('id', 'page-'+window.pageCount.get());
+  window.pageCount.add(1)
   paper.appendChild(page_clone);
 }
