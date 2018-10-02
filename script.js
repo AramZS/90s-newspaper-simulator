b6793dfd1c5980b23c6b3467eb32a40e0c49f253
Glitch (newspaper-business-simulator) <none>
2018-10-02 13:53:01 +0000

    💝🐷 Checkpoint ./script.js:1908428/1088 ./style.css:1908428/30

--- script.js
+++ script.js
@@ -147,8 +147,16 @@ function setGrid(pageGrid, gridWidth, gridHeight){
   });
 }
 
-function rotateStorySize(){
+function rotateStorySize(e){
 
+  if (e.classList.contains('story-default')){
+    e.classList.replace('story-default', 'story-skinny');
+  } else if (e.classList.contains('story-skinny')) {
+      e.classList.replace('story-skinny', 'story-wide');    
+  } else if (e.classList.contains('story-wide')) {
+    e.classList.replace('story-wide', 'story-default');   
+  } else { e.classList.add('story-default') }
+          
 }
 
 function shipPaper(){
@@ -207,8 +215,11 @@ function breakEven(iteration){
 
 function articleTransformer(){
   var toolgrid = document.getElementById('toolbox_grid');
-  toolgrid.addEventListener("mouseup", function(e){
-    if (e.pat){}
+  toolgrid.addEventListener("click", function(e){
+    if (e.path[0].classList.contains('story')){
+      console.log('rotate', e.path[0]);
+      rotateStorySize(e.path[0]);
+    }
   });
 }
 
@@ -272,11 +283,11 @@ function scriptInit(){
 
 function trackPageDrags(){
         window.draggableObjects.mainObject.on('drag:over', function(e){
-          console.log(e);
+          //console.log(e);
         });
   
         window.draggableObjects.mainObject.on('drag:over:container', function(e){
-          console.log('toolboxDrag Over Container', e);
+          //console.log('toolboxDrag Over Container', e);
 
           window.draggableObjects.previouslyOver = e.data.originalSource;
           var oldClassname = JSON.stringify(e.data.overContainer.className);
@@ -288,7 +299,7 @@ function trackPageDrags(){
             if ('toolbox_grid' !== e.data.overContainer.id){
               var rowCount = (window.getComputedStyle(e.data.overContainer)['grid-template-rows']).split(' ').length;
               var colCount = (window.getComputedStyle(e.data.overContainer)['grid-template-columns']).split(' ').length;
-              console.log('rows', rowCount, 'cols', colCount);
+              //console.log('rows', rowCount, 'cols', colCount);
               if ((rowCount > 3) || (colCount > 3)){
                 console.log('drag attempt rejected');
                 document.getElementById('toolbox_grid').appendChild(e.data.source);
